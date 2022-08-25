a5d191b24be2c1a0caf405d1b9ee435dacc8b484
ddatunashvili <davitidatunashvili98@gmail.com>
2022-08-26 01:45:24 +0400

    floating

--- Readme.md
+++ Readme.md
@@ -6,6 +6,7 @@
 | **2**  | [Twiscode - how we work](https://github.com/ddatunashvili/Design_Templates/tree/master/Twiscode-how-we-work "Twiscode - how we work") |[web link](https://ddatunashvili.github.io/Design_Templates/Twiscode-how-we-work/index.html)|
 | **3**  | [Header](https://github.com/ddatunashvili/Design_Templates/tree/master/Header "Header") |[web link](https://ddatunashvili.github.io/Design_Templates/Header/index.html)|
 | **4**  | [simple caroosel](https://github.com/ddatunashvili/Design_Templates/tree/master/simple_caroosel "simple caroosel") |[web link](https://ddatunashvili.github.io/Design_Templates/simple_caroosel/index.html)|
+| **5**  | [floating_anime](https://github.com/ddatunashvili/Design_Templates/tree/master/floating_anime "floating_anime") |[web link](https://ddatunashvili.github.io/Design_Templates/floating_anime/index.html)|
 
 
 
---
+++ floating_anime/script.js
@@ -0,0 +1,74 @@
+
+// გვერდი რომ ჩაიტვირთება გაეშვას
+window.onload = (event) => {
+    // რენდომ sample. სიიდან რენდომად 3 ელემენტის ამოღება
+    function getRandom(arr, n) {
+      var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+      if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+      while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+      }
+      return result;
+    }
+    
+    // ანიმეების სია
+    var anime_list = [
+      {
+        name: "მაგიური ბრძოლა",
+        url: "file.ge",
+        pic: "https://geomovie.ge/uploads/posts/2022-02/1643675879_1637230665_magiuri-brdzola-qartulad-anime-qartulad.webp",
+      },
+      {
+        name: "ვან პისი (ქართულად) / One Piece (qartulad)",
+        url: "file.ge",
+        pic: "https://mykadri.com/uploads/posts/2022-06/1654721759_one-piece-geo-poster.jpg",
+      },
+      {
+        name: "ბორუტო ქართულად / Boruto qartulad   ",
+        url: "file.ge",
+        pic: "https://mykadri.com/uploads/posts/2022-03/1648206877_boruto-poster.webp",
+      }
+    ];
+    
+    // ანიმეების ამოღება
+    var anime_list = getRandom(anime_list, 3);
+    
+    // html ში ჩასმა ანიმეების
+    var animes = document.querySelector(".animes");
+    anime_list.forEach(function (anime) {
+      animes.innerHTML += `<div class="anime"><div class="popup"><p>${anime["name"]}</p></div><a href="${anime["url"]}"><img src='${anime["pic"]}' ></a> </div>`;
+    });
+    
+    // activate img 
+    $(function () {
+      $(".anime ").mouseover(function (el) {
+        this.classList.add("active");
+        check();
+      });
+      $(".anime ").mouseleave(function (el) {
+        this.classList.remove("active");
+        this.querySelector(".popup");
+        check();
+      });
+    });
+    
+    // თუ ანიმე აქტივია პოპაპიც აქტივზე იყოს
+    function check() {
+      var animes = document.querySelectorAll(".anime");
+      animes.forEach(function (el) {
+        try {
+          el.querySelector(".active .popup").classList.add("active");
+        } catch {
+            el.querySelector(".popup").classList.remove("active");
+        }
+    
+      });
+    }
+    
+    
+    } // onload
